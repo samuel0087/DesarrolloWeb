c34103c546b60133560538b57fe72a4b23073c50
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-07-30 19:54:33 -0300

    eventos de mouse

--- Web/JavaScript/22-eventos/main.js
+++ Web/JavaScript/22-eventos/main.js
@@ -7,3 +7,29 @@ boton.addEventListener("click", (event) => {
 
     console.log(event.clientX);
 })
+
+let caja = document.querySelector("#caja");
+
+document.querySelector("#botonDoble").addEventListener("dblclick", () => {
+    //alert("Preciono un doble chlick");
+    caja.classList.toggle("oculto");
+
+})
+
+caja.addEventListener("mouseover", () => {
+    caja.style.background = "blue";
+})
+
+caja.addEventListener("mouseout", () => {
+    caja.style.background = "red";
+    caja.style.border = "none";
+    b = 0;
+})
+
+let b = 0;
+
+caja.addEventListener("mousemove", () => {
+    caja.style.border = b + "px solid black";
+    b++;
+})
+
