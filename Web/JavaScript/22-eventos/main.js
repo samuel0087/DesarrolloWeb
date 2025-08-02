3ccd94601caf6ae9855fc5b8dfba5ca593e64179
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-08-02 02:01:07 -0300

    Terminado la seccion de eventos

--- Web/JavaScript/22-eventos/main.js
+++ Web/JavaScript/22-eventos/main.js
@@ -56,3 +56,70 @@ texto.addEventListener("keypress", () => {
     console.warn(event.key);
 })
 
+//Eventos de formulario
+let formulario = document.querySelector("#superFormulario");
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let nombre = document.querySelector("#name").value;
+    let correo = document.querySelector("#correo").value;
+    let genero = document.querySelector("#genero").value;
+
+    caja.innerHTML =`<p>Nombre: ${nombre}</p> <p>Correo:${correo}</p> <p>Genero: ${genero}</p>`;
+})
+
+//INPUTS
+/*
+ *IMPORTANTE: Al usar funciones de flecha " () => {} ",no se puede acceder al valor del mismo por medio de this.
+ *En cambio si usamos " Function(){} " podremos usarlo. 
+ */
+
+let inputNombre = document.querySelector("#name");
+
+inputNombre.addEventListener("input", function(){
+    console.log(`Text: ${this.value}`)
+})
+
+let generos = document.querySelector("#genero");
+
+generos.addEventListener("change", ()=>{
+    console.warn(`Genero: ${generos.value}`)
+})
+
+//Focus y Blur
+
+let email = document.querySelector("#correo");
+
+email.addEventListener("focus", ()=>{
+    console.warn("Recuerda que el email tiene que tener un @ y un .com");
+})
+
+
+email.addEventListener("blur", ()=>{
+    if(!email.value.includes("@")){
+        console.error("Usted no aprende,¿Verdad?");
+    }
+    else{
+        console.log("Puede prosegir");
+    }
+})
+
+//Eventos del socumento y la ventana
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("Se cargo el documento");
+})
+
+window.addEventListener("load", ()=>{
+    console.warn("Se cargo la web completa");
+})
+
+window.addEventListener("resize", () => {
+    console.error(`Ancho: ${window.innerWidth}  Alto: ${window.innerHeight}`)
+})
+
+
+window.addEventListener("scroll", ()=>{
+    console.log(`Estas en el pixel: ${window.scrollY}`)
+})
