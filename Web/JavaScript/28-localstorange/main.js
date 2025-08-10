357db72dcf73e103a2f1f178abed2e231147d608
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-08-10 00:49:01 -0300

    Terminada la seccion de localstorage

--- Web/JavaScript/28-localstorange/main.js
+++ Web/JavaScript/28-localstorange/main.js
@@ -19,4 +19,32 @@ window.addEventListener("load",()=>{
     //Sacar datos
     console.log(localStorage.getItem("curso"));
     console.log(localStorage.getItem("enlace"));
+
+    //Guardar objetos
+    let animal = {
+        especie: "Elefante",
+        nombre: "Estanpi",
+        color: "Gris"
+    };
+
+    localStorage.setItem("animal", JSON.stringify(animal));
+
+    //Recuperar objeto
+
+    let animalJS = JSON.parse(localStorage.getItem("animal"));
+
+    console.log(animalJS);
+
+    //Eliminar elementos
+    localStorage.removeItem("animal");
+
+
+
+    //vaciar storage
+
+    document.querySelector("#vaciar").addEventListener("click", ()=>{
+        localStorage.clear();
+    })
+
 })
+
