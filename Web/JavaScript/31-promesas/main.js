685eddbf26c7e825776a9ac6017599b360aab5f5
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-09-18 00:33:00 -0300

    Finalizando la seccion de promesas

--- Web/JavaScript/31-promesas/main.js
+++ Web/JavaScript/31-promesas/main.js
@@ -1,30 +1,87 @@
-//Promesas
-const miPromesa = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let correcto = true;
+// //Promesas
+// const miPromesa = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let correcto = true;
 
-        if(correcto){
-            let miObjeto = {
-                nombre: "El justiciero",
-                genero: "Accion",
-                anio: 2020
-            }
+//         if(correcto){
+//             let miObjeto = {
+//                 nombre: "El justiciero",
+//                 genero: "Accion",
+//                 anio: 2020
+//             }
 
-            resolve(miObjeto);
-        }
-        else{
-            reject("No se consiguio mi objeto");
-        }
+//             resolve(miObjeto);
+//         }
+//         else{
+//             reject("No se consiguio mi objeto");
+//         }
 
-    }, 3500)
-})
+//     }, 3500)
+// })
 
-miPromesa
-    .then(result => {
-        console.warn(result);
+// miPromesa
+//     .then(result => {
+//         console.warn(result);
+//     })
+//     .catch(reject  =>{
+//         console.error(reject);
+//     })
+
+// console.log("Hollaa");
+
+// //Promesas en cadena
+
+function servirPizza(){
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Pizza servida"), 15000);
+    });
+}
+
+function servirHamburguesa(){
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Hamburguesa servida"), 8000);
+    });
+}
+
+function servirLomito(){
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Lomito servido"), 5000);
     })
-    .catch(reject  =>{
-        console.error(reject);
-    })
+}
+/*
+// servirPizza()
+//     .then(resultado => {
+//         console.log(resultado);
 
-console.log("Hollaa");
+//         return servirHamburguesa();
+//     })
+//     .then(resultado2 => {
+//         console.log(resultado2);
+
+//         return servirLomito();
+//     })
+//     .then(resultado3 => {
+//         console.log(resultado3);
+//     })
+*/
+
+//Asybc y await
+
+async function servirPlatos(){
+    try{
+        let resultado1 = await servirPizza();
+        console.log(resultado1);
+
+        let resultado2 = await servirHamburguesa();
+        console.log(resultado2);
+
+        let resultado3 = await servirLomito();
+        console.log(resultado23);
+    }
+    catch(error){
+        console.log("Error: " + error);
+    }
+}
+
+
+servirPlatos();
