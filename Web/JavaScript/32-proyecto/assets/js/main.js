d6909fc1f95a59a77befc06923e0c154914ad506
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-09-28 01:12:14 -0300

    Se realizo el slider de imagenes

--- Web/JavaScript/32-proyecto/assets/js/main.js
+++ Web/JavaScript/32-proyecto/assets/js/main.js
@@ -1,7 +1,6 @@
 let btn = document.querySelector(".btn-top");
 let pagina = document.querySelector("body");
 
-
 btn.addEventListener("click", () => {
     pagina.scrollIntoView({behavior: "smooth", block: "start"});
 })
@@ -14,3 +13,32 @@ window.addEventListener("scroll",()=>{
         btn.classList.add("oculto");
     }
 })
+
+//Slider de imagenes
+let imagenes = [
+    "./assets/img/hojas.jpg",
+    "./assets/img/cielo.jpg",
+    "./assets/img/rojo.jpg"
+];
+
+let banner= document.querySelector(".banner__img");
+let btnSiguiente = document.querySelector(".banner__arrowRight");
+let btnAnterior = document.querySelector(".banner__arrowLeft");
+
+btnSiguiente.addEventListener("click", ()=>{
+    let imagenActual = banner.getAttribute("src");
+    let indiceActual = imagenes.indexOf(imagenActual);
+
+    if(indiceActual < imagenes.length-1){
+        banner.setAttribute("src", imagenes[indiceActual+1]);
+    }
+});
+
+btnAnterior.addEventListener("click", ()=>{
+    let imagenActual = banner.getAttribute("src");
+    let indiceActual = imagenes.indexOf(imagenActual);
+
+    if(indiceActual > 0){
+        banner.setAttribute("src", imagenes[indiceActual-1]);
+    }
+});
